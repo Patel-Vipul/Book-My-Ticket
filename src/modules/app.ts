9a5a220aa2cccc18fc26bc6a52f486831f37aeab
Patel-Vipul <patelvipul9601@gmail.com>
2026-07-17 02:06:19 +0530

    refactor: configured cors

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -3,7 +3,7 @@ import type { Express } from "express";
 import authRouter from "./auth/auth.route.js";
 import errorHandler from "../common/middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 import movieRouter from "./movie/movie.route.js";
 import bookingRouter from "./booking/booking.route.js";
 
@@ -11,21 +11,26 @@ const expressApplication = (): Express => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cookieParser())
-  app.use(cors({
-    origin : "book-my-tickett.netlify.app",
-    credentials : true
-  }))
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: [
+        "https://book-my-tickett.netlify.app",
+        "https://spontaneous-klepon-eaa5bf.netlify.app",
+      ],
+      credentials: true,
+    }),
+  );
   //auth router
   app.use("/auth", authRouter);
 
   //movie-router
-  app.use("/movies",movieRouter)
+  app.use("/movies", movieRouter);
 
   //booking-router
-  app.use("/bookings", bookingRouter)
+  app.use("/bookings", bookingRouter);
 
-  app.use(errorHandler)
+  app.use(errorHandler);
 
   return app;
 };
