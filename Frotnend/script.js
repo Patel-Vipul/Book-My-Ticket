1a9ebd2b4c578ad483973670a91bffe5def989bd
Patel-Vipul <patelvipul9601@gmail.com>
2026-07-17 01:45:25 +0530

    refactor: added render url

--- Frotnend/script.js
+++ Frotnend/script.js
@@ -1,5 +1,5 @@
 // ── CONFIG ────────────────────────────────────────────────────
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://book-my-ticket-yz80.onrender.com";
 
 // ── STATE ─────────────────────────────────────────────────────
 const state = {
